7fac974d8d02478f6ef2ccbe3017bcfd0118bac8
thierno <thiernoibrahimalo@esp.sn>
2022-03-14 11:27:36 +0000

    remove dependencis folder

--- backend/app/core/celery_app.py
+++ backend/app/core/celery_app.py
@@ -1,5 +1,6 @@
 from celery import Celery
+from app.core import config
 
-celery_app = Celery("worker", broker="redis://redis:6379/0")
+celery_app = Celery("worker", broker=config.REDIS_URL)
 
 celery_app.conf.task_routes = {"app.tasks.*": "main-queue"}
--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -5,3 +5,5 @@ PROJECT_NAME = "cura"
 SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
 
 API_V1_STR = "/api/v1"
+
+REDIS_URL = os.getenv("REDIS_URL")
